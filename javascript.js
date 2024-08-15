014b9f31a059432c596cde72a7b66670a5db3f1d
DK32093 <107143112+DK32093@users.noreply.github.com>
2024-08-15 17:27:52 -0400

    Write operate function

--- javascript.js
+++ javascript.js
@@ -15,3 +15,20 @@ function multiply (a, b) {
 function divide (a, b) {
     return a / b;
 };
+
+// Calculator operation variables
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate (num1, op, num2) {
+    if (op === "+") {
+        return add(num1, num2);
+    } else if (op === "-") {
+        return subtract(num1, num2);
+    } else if (op === "*") {
+        return multiply(num1, num2);
+    } else {
+        return divide(num1, num2);
+    };
+};
